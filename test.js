e197ccee9fe77565948dace1449fc2c0b05308cf
Mindrescu Marina <mindrescum2011c@gmail.com>
2022-08-10 14:45:57 +0300

    add function close/open elements header

---
+++ test.js
@@ -0,0 +1,20 @@
+
+window.onload = function () {
+    document.addEventListener("click", OpenMenu);
+
+    
+    function OpenMenu(e) {
+        const targetElement = e.target;
+        if (targetElement.classList.contains('menu__arrow')) {
+        targetElement.closest('.menu__item').classList.toggle('_hover');
+        } else if (!targetElement.closest('.menu__item') && document.querySelectorAll('.menu__item._hover').length > 0) {
+            document.querySelectorAll('.menu__item').forEach(item => item.classList.remove('_hover'));
+        } 
+        
+        if (targetElement.classList.contains('search-form__icon')) {
+            document.querySelector('.search-form').classList.toggle('_active');
+        } else if (!targetElement.closest('.search-form')) {
+            document.querySelector('.search-form').classList.remove('_active');
+        } 
+    }
+}
